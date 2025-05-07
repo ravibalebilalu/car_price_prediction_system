ee0f50fb2373df082c95bfb6535d1839f49ab8c4
ravibalebilalu <81ravikiran@gmail.com>
2025-05-07 15:51:38 +0530

    final

--- app.py
+++ app.py
@@ -17,41 +17,30 @@ def prediction(data):
     scaled = scaler.transform(combiined)
     return model.predict(scaled)
 
+make = ['Dodge' , 'other', 'Volkswagen', 'Nissan', 'Toyota', 'Chevrolet','Mazda', 'Honda', 'Ford', 'GMC']
+models = ['other', 'Tundra', 'Frontier', 'GTI', 'Silverado 1500', 'Tacoma','Sierra 1500', 'F-150', 'Beetle Convertible', 'Accord']
+engine_type = ['premium unleaded (recommended)', 'regular unleaded', 'diesel','premium unleaded (required)', 'flex-fuel (unleaded/E85)','flex-fuel (premium unleaded recommended/E85)', 'electric','flex-fuel (premium unleaded required/E85)','flex-fuel (unleaded/natural gas)', 'natural gas']
+transmission_type = ['MANUAL', 'AUTOMATIC', 'AUTOMATED_MANUAL', 'UNKNOWN','DIRECT_DRIVE']
+driven_wheels = ['rear wheel drive', 'front wheel drive', 'all wheel drive','four wheel drive']
+vehicle_style = ['Coupe', 'Sedan', '4dr SUV', '2dr Hatchback', 'Convertible','Wagon', 'other', 'Crew Cab Pickup', 'Extended Cab Pickup','4dr Hatchback']
+vehicle_size = ['Large', 'Compact', 'Midsize']
+year = [year for year in range(1990,2018)]
+engine_cylinders = [ 1,3,4,5,6,8,9]
+
 app = Flask(__name__)
+
+
+
  
 @app.route("/")
 def home():
-    make = ['Dodge' , 'other', 'Volkswagen', 'Nissan', 'Toyota', 'Chevrolet','Mazda', 'Honda', 'Ford', 'GMC']
-    models = ['other', 'Tundra', 'Frontier', 'GTI', 'Silverado 1500', 'Tacoma','Sierra 1500', 'F-150', 'Beetle Convertible', 'Accord']
-    engine_type = ['premium unleaded (recommended)', 'regular unleaded', 'diesel','premium unleaded (required)', 'flex-fuel (unleaded/E85)','flex-fuel (premium unleaded recommended/E85)', 'electric','flex-fuel (premium unleaded required/E85)','flex-fuel (unleaded/natural gas)', 'natural gas']
-    transmission_type = ['MANUAL', 'AUTOMATIC', 'AUTOMATED_MANUAL', 'UNKNOWN','DIRECT_DRIVE']
-    driven_wheels = ['rear wheel drive', 'front wheel drive', 'all wheel drive','four wheel drive']
-    vehicle_style = ['Coupe', 'Sedan', '4dr SUV', '2dr Hatchback', 'Convertible','Wagon', 'other', 'Crew Cab Pickup', 'Extended Cab Pickup','4dr Hatchback']
-    vehicle_size = ['Large', 'Compact', 'Midsize']
-    year = [year for year in range(1990,2018)]
-    engine_cylinders = [ 1,3,4,5,6,8,9]
-    
     return render_template("home.html",make_list = make,model_list = models,engine_type_list = engine_type,transmission_type_list = transmission_type,driven_wheels_list = driven_wheels,vehicle_style_list=vehicle_style ,vehicle_size_list=vehicle_size,year_list = year,engine_cylinders_list=engine_cylinders)
 
 
 @app.route("/predict",methods=['POST'])
 def predict():
-    make = ['Dodge' , 'other', 'Volkswagen', 'Nissan', 'Toyota', 'Chevrolet','Mazda', 'Honda', 'Ford', 'GMC']
-    models = ['other', 'Tundra', 'Frontier', 'GTI', 'Silverado 1500', 'Tacoma','Sierra 1500', 'F-150', 'Beetle Convertible', 'Accord']
-    engine_type = ['premium unleaded (recommended)', 'regular unleaded', 'diesel','premium unleaded (required)', 'flex-fuel (unleaded/E85)','flex-fuel (premium unleaded recommended/E85)', 'electric','flex-fuel (premium unleaded required/E85)','flex-fuel (unleaded/natural gas)', 'natural gas']
-    transmission_type = ['MANUAL', 'AUTOMATIC', 'AUTOMATED_MANUAL', 'UNKNOWN','DIRECT_DRIVE']
-    driven_wheels = ['rear wheel drive', 'front wheel drive', 'all wheel drive','four wheel drive']
-    vehicle_style = ['Coupe', 'Sedan', '4dr SUV', '2dr Hatchback', 'Convertible','Wagon', 'other', 'Crew Cab Pickup', 'Extended Cab Pickup','4dr Hatchback']
-    vehicle_size = ['Large', 'Compact', 'Midsize']
-    year = [year for year in range(1990,2018)]
-    engine_cylinders = [ 1,3,4,5,6,8,9]
-
     data =  [x for x in request.form.values()]
-    
     result = prediction(data)[0]
-     
-     
-    
     return render_template("home.html" , result=round(result,2),make_list = make,model_list = models,engine_type_list = engine_type,transmission_type_list = transmission_type,driven_wheels_list = driven_wheels,vehicle_style_list=vehicle_style ,vehicle_size_list=vehicle_size,year_list = year,engine_cylinders_list=engine_cylinders)
 
 if __name__ == "__main__":
